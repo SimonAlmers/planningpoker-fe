09b4a9c4c0d5ca33e293a5aab455fd8d4a6a24e6
Simon Almers <account@simonalmers.com>
2021-06-21 11:07:41 +0200

    Add Page Titles

--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Head from "next/head";
 
 const SettingsView = (): JSX.Element => (
   <div className="bg-gray-900 h-screen pt-48 flex justify-center text-white">
-    <h1 className="text-3xl">Settings</h1>
+    <Head>
+      <title>Settings | Planning Poker</title>
+    </Head>
+    <h1 className="text-3xl font-bold">Settings</h1>
   </div>
 );
 export default SettingsView;
